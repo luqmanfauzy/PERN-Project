65f072452032d62fa6df15eadd3541dfdba9e01c
luqman fauzy <luqmannfauzy46@gmail.com>
2025-09-30 16:08:09 +0800

    first commit, build scaffold

---
+++ controllers/employee.js
@@ -0,0 +1,15 @@
+export async function getEmployees(req, res, next) {
+    //
+}
+export async function getSpecifiedEmployees(req, res, next) {
+    //
+}
+export async function createEmployees(req, res, next) {
+    //
+}
+export async function editEmployees(req, res, next) {
+    //
+}
+export async function deleteEmployees(req, res, next) {
+    //
+}
---
+++ routes/employeeRoutes.js
@@ -0,0 +1,21 @@
+import express from "express";
+const router = express.Router();
+import {
+  getEmployees,
+  getSpecifiedEmployees,
+  createEmployees,
+  editEmployees,
+  deleteEmployees,
+} from "./controllers/employee.js";
+
+router.get("/", getEmployees);
+
+router.post("/", createEmployees);
+
+router.get("/:id", getSpecifiedEmployees);
+
+router.put("/:id", editEmployees);
+
+router.delete("/:id", deleteEmployees);
+
+export default router;
